e4e971c75a05235a7cf2215ccb2cafc243cbea7b
d-cap <davide.capomagi@gmail.com>
2024-03-02 16:14:17 +0000

    Added plot for past days

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 
 use std::{
     cell::RefCell,
-    collections::HashMap,
+    collections::{BTreeMap, HashMap},
+    hash::Hash,
     path::{Path, PathBuf},
     rc::Rc,
     sync::{Arc, Mutex},
@@ -11,8 +12,9 @@ use std::{
 
 use active_win_pos_rs::{get_active_window, ActiveWindow, WindowPosition};
 use device_query::{DeviceQuery, DeviceState, Keycode, MouseState};
-use eframe::egui::{self, Layout};
+use eframe::egui::{self, Layout, Ui};
 use egui_extras::{Column, TableBuilder};
+use egui_plot::{BarChart, Plot};
 use serde::{Deserialize, Serialize};
 
 const DEFAULT_OUTPUT_DIRECTORY: &str = "~/.time_back";
@@ -58,6 +60,7 @@ fn main() -> Result<(), eframe::Error> {
     };
 
     let window_time = Arc::new(Mutex::new(data));
+    let plot_data = collect_previous_data(&cfg.output_directory, &file_name).unwrap_or_default();
     let config = Arc::new(Mutex::new(cfg));
     {
         let window_time = window_time.clone();
@@ -110,7 +113,6 @@ fn main() -> Result<(), eframe::Error> {
                     last_save = Instant::now();
                     let data = window_time.lock().unwrap().clone();
                     let output_directory = config.lock().unwrap().output_directory.clone();
-                    let file_name = generate_file_name();
                     match std::fs::File::create(output_directory.to_owned() + "/" + &file_name) {
                         Ok(f) => {
                             if let Err(e) = serde_json::to_writer(f, &data) {
@@ -129,6 +131,7 @@ fn main() -> Result<(), eframe::Error> {
         let window_time = window_time.clone();
         let config = config.clone();
         let close_inner = close.clone();
+        let plot_data = plot_data.clone();
         eframe::run_native(
             "Time back!",
             options.clone(),
@@ -137,6 +140,8 @@ fn main() -> Result<(), eframe::Error> {
                     window_time,
                     config,
                     close: close_inner,
+                    show_plot: false,
+                    chart: plot_data,
                 })
             }),
         )?;
@@ -145,6 +150,34 @@ fn main() -> Result<(), eframe::Error> {
         }
     }
     Ok(())
+}
+
+fn collect_previous_data(
+    output_directory: &str,
+    current_file: &str,
+) -> Result<Vec<egui_plot::Bar>, std::io::Error> {
+    let current_file = output_directory.to_owned() + "/" + current_file;
+    let mut result: BTreeMap<String, Duration> = BTreeMap::new();
+    for entry in std::fs::read_dir(Path::new(&output_directory))? {
+        let entry = entry?;
+        let p_entry = entry.path();
+        let s_entry = p_entry.to_str().unwrap_or_default();
+        if current_file != s_entry {
+            if let Ok(f) = std::fs::File::open(entry.path()) {
+                let data: HashMap<String, Duration> =
+                    serde_json::from_reader(f).unwrap_or_default();
+                for (k, v) in data {
+                    *result.entry(k).or_default() += v;
+                }
+            }
+        }
+    }
+    let result = result
+        .into_iter()
+        .enumerate()
+        .map(|(i, (k, v))| egui_plot::Bar::new(i as f64, v.as_secs_f64()).name(k))
+        .collect::<Vec<_>>();
+    Ok(result)
 }
 
 fn generate_file_name() -> String {
@@ -158,6 +191,8 @@ struct TimeBack {
     window_time: Arc<Mutex<HashMap<String, Duration>>>,
     config: Arc<Mutex<Config>>,
     close: Rc<RefCell<bool>>,
+    show_plot: bool,
+    chart: Vec<egui_plot::Bar>,
 }
 
 impl Drop for TimeBack {
@@ -192,71 +227,93 @@ impl eframe::App for TimeBack {
                 });
             });
 
-            if let Ok(mut config) = self.config.lock() {
-                if config.configured {
-                    let table_height = 20.;
-                    let table = TableBuilder::new(ui)
-                        .striped(true)
-                        .resizable(false)
-                        .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
-                        .column(Column::auto())
-                        .column(Column::remainder())
-                        .min_scrolled_height(0.0);
-                    if let Ok(map) = self.window_time.lock() {
-                        table.body(|mut body| {
-                            let mut overall = Duration::new(0, 0);
-                            for (n, d) in map.iter() {
-                                body.row(table_height, |mut row| {
-                                    row.col(|ui| {
-                                        ui.label(n);
-                                    });
-                                    row.col(|ui| {
-                                        ui.label(humantime::Duration::from(*d).to_string());
-                                    });
-                                    overall += *d;
-                                })
-                            }
-                            body.row(table_height, |mut row| {
-                                row.col(|_ui| {});
-                                row.col(|_ui| {});
-                            });
-                            body.row(table_height, |mut row| {
-                                row.col(|ui| {
-                                    ui.label("Overall");
-                                });
-                                row.col(|ui| {
-                                    ui.label(humantime::Duration::from(overall).to_string());
-                                });
-                            })
-                        });
-                    }
-                } else {
-                    if ui.button("Select output directory").clicked() {
-                        config.output_directory = match tinyfiledialogs::select_folder_dialog(
-                            "Select output directory",
-                            "",
-                        ) {
-                            Some(result) => result,
-                            None => DEFAULT_OUTPUT_DIRECTORY.to_string(),
-                        };
-                    }
-                    ui.label(format!(
-                        "Current output directory: {}",
-                        config.output_directory
-                    ));
-                    if ui.button("Accept").clicked() {
-                        config.configured = true;
-                        match confy::store("time_back", None, &*config) {
-                            Ok(_) => {}
-                            Err(_) => {
-                                config.configured = false;
-                                ui.label("Error saving the configuration");
-                            }
-                        }
-                    }
+            let configured = if let Ok(mut config) = self.config.lock() {
+                if !config.configured {
+                    display_configuration(ui, &mut config)
                 }
+                config.configured
+            } else {
+                false
+            };
+            if configured {
+                self.display_main_ui(ui);
             }
         });
         ctx.request_repaint();
+    }
+}
+
+impl TimeBack {
+    fn display_main_ui(&mut self, ui: &mut Ui) {
+        let table_height = 20.;
+        let table = TableBuilder::new(ui)
+            .striped(true)
+            .resizable(false)
+            .cell_layout(egui::Layout::left_to_right(egui::Align::Center))
+            .column(Column::auto())
+            .column(Column::remainder())
+            .min_scrolled_height(0.0);
+        if let Ok(map) = self.window_time.lock() {
+            table.body(|mut body| {
+                let mut overall = Duration::new(0, 0);
+                for (n, d) in map.iter() {
+                    body.row(table_height, |mut row| {
+                        row.col(|ui| {
+                            ui.label(n);
+                        });
+                        row.col(|ui| {
+                            ui.label(humantime::Duration::from(*d).to_string());
+                        });
+                        overall += *d;
+                    })
+                }
+                body.row(table_height, |mut row| {
+                    row.col(|_ui| {});
+                    row.col(|_ui| {});
+                });
+                body.row(table_height, |mut row| {
+                    row.col(|ui| {
+                        ui.label("Overall");
+                    });
+                    row.col(|ui| {
+                        ui.label(humantime::Duration::from(overall).to_string());
+                    });
+                })
+            });
+        }
+        ui.separator();
+        if ui.button("Show plot").clicked() {
+            self.show_plot = !self.show_plot;
+        }
+        if self.show_plot {
+            ui.add_space(5.);
+            Plot::new("Average").show(ui, |plot_ui| {
+                plot_ui.bar_chart(BarChart::new(self.chart.clone()))
+            });
+        }
+    }
+}
+
+fn display_configuration(ui: &mut Ui, config: &mut Config) {
+    if ui.button("Select output directory").clicked() {
+        config.output_directory =
+            match tinyfiledialogs::select_folder_dialog("Select output directory", "") {
+                Some(result) => result,
+                None => DEFAULT_OUTPUT_DIRECTORY.to_string(),
+            };
+    }
+    ui.label(format!(
+        "Current output directory: {}",
+        config.output_directory
+    ));
+    if ui.button("Accept").clicked() {
+        config.configured = true;
+        match confy::store("time_back", None, &*config) {
+            Ok(_) => {}
+            Err(_) => {
+                config.configured = false;
+                ui.label("Error saving the configuration");
+            }
+        }
     }
 }
